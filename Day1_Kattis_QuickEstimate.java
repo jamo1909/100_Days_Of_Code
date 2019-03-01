a09ce6614700388ad8172713e403485841a62682
James Murphy <rjmjmurphy@gmail.com>
2019-03-01 23:14:42 +0000

    Create Day1_Kattis_QuickEstimate.java

---
+++ Day1_Kattis_QuickEstimate.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Day1_Kattis_QuickEstimate {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in);
+		
+		int num = input.nextInt()+1;
+		String estimate;
+		int results[] = new int[num];
+		
+		for(int i=0; i<num; i++) {
+			 estimate = input.nextLine();
+			 results[i] = estimate.length();
+		}
+		
+		 //Arrays.stream(results).forEach(System.out::println);
+		
+		for(int i=1; i<num; i++) {
+			System.out.println(results[i]);
+		}
+		 input.close();
+	}
+
+}
