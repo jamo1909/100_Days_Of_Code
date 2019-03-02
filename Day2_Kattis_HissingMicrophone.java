84f339f77ec8b4b716de237ef8bc115b8074e7d4
jamo1909 <rjmjmurphy@gmail.com>
2019-03-02 00:18:31 +0000

    ADD

---
+++ Day2_Kattis_HissingMicrophone.java
@@ -0,0 +1,16 @@
+package daysOfCode;
+import java.util.*;
+public class Day2_Kattis_HissingMicrophone {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		String check = ("ss").toLowerCase();
+		String test = (input.nextLine()).toLowerCase();
+		
+		
+		test.contains(test);
+
+	}
+
+}
